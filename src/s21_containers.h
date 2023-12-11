960becb73538b0c088195ea160bf9a527f435cfb
Superboy <zakharenkolink@gmail.com>
2023-12-11 04:29:48 +0300

    init s12_vector

---
+++ src/s21_containers.h
@@ -0,0 +1,12 @@
+#ifndef S21_CONTAINER_SRC_S21_CONTAINER_H_
+#define S21_CONTAINER_SRC_S21_CONTAINER_H_
+
+// typedef class s21_ {
+//     private:
+        
+//     public:
+
+//     protected:
+// } s21_map;
+
+#endif  // S21_CONTAINER_SRC_S21_CONTAINER_H_
