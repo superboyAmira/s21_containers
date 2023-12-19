cf1a9aa9eb7177373403a35fed1a895c1fca490a
baggmela <rvylarr@gmail.com>
2023-12-19 07:10:12 +0300

    push, pop

--- list/s21_list.cpp
+++ list/s21_list.cpp
@@ -1,10 +1,99 @@
-
 #include "s21_list.h"
 
 namespace s21 {
 
-    S21list::S21list() {
+    // === constructors === //
+    template<typename T>
+    S21list<T>::S21list() : size_(0), head_(nullptr), tail_(nullptr), end_(nullptr) {
+        this->end_ = new Node();
+    }
 
+    template<typename T>
+    S21list<T>::S21list(size_t size) : size_(size), end_(nullptr) {
+        if(size < 0) throw std::out_of_range("Error: Invalid size of created list");
+        else if(size == 0) this->end_ = new Node();
+        else {
+            for(auto i = 0; i < size; i++)
+                this->push_back(nullptr);
+        }
+    }
+
+    template<typename T>
+    S21list<T>::~S21list() {
+        while(!empty()) {
+            pop_back();
+        }
+        delete end_;
+    }
+
+    // TODO: непонятно, в какой позиции находится end_
+    // === methods === //
+    template<typename T>
+    void S21list<T>::push_front(T value) {
+        Node *new_node = new Node(value);
+        if(empty()) {
+            head_ = new_node;
+            tail_ = new_node;
+        } else {
+            head_->prev_ = new_node;
+            new_node->next_ = head_;
+            head_ = new_node;
+        }
+        size_++;
+    }
+
+    template<typename T>
+    void S21list<T>::push_back(T value) {
+        Node *new_node = new Node(value);
+        if(empty()) {
+            head_ = new_node;
+            tail_ = new_node;
+        } else {
+            tail_->next_ = new_node;
+            new_node->prev_ = tail_;
+            tail_ = new_node;
+        }
+        size_++;
+    }
+
+    template<typename T> // ну и булдыга конечно
+    void S21list<T>::pop_front() {
+        if(empty()) throw std::out_of_range("Error: List is empty");
+        if(size_ == 1) {
+            delete head_;
+            tail_ = nullptr;
+        } else {
+            Node *new_head = head_->next_;
+            head_->next_->prev_ = nullptr;
+            delete head_;
+            head_ = new_head;
+        }
+        size_--;
+    }
+
+    template<typename T>
+    void S21list<T>::pop_back() {
+        if(empty()) throw std::out_of_range("Error: List is empty");
+        if(size_ == 1) {
+            delete tail_;
+            head_ = nullptr;
+        } else {
+            Node *new_tail = tail_->prev_;
+            tail_->prev_->next_ = nullptr;
+            delete tail_;
+            tail_ = new_tail;
+        }
+        size_--;
+    }
+
+    template<typename T>
+    size_t S21list<T>::size() {
+        return size_;
+    }
+
+    template<typename T>
+    bool S21list<T>::empty() {
+        return head_ == nullptr && tail_ == nullptr;
     }
 
 } // namespace s21
--- list/s21_list.h
+++ list/s21_list.h
@@ -2,43 +2,74 @@
 #define CPP_S21_CONTAINERS_SRC_S21_LIST_H
 
 #include <iostream>
+#include <initializer_list>
 
 namespace s21 {
-    // TODO
-    // Конструкторы деструкторы
-    // front back - получение крайних элементов
-    // size - размер списка
-    // ? empty - пустой ли список (true/false)
-    /* resize - изменение размера списка
-     *  (n) - простое изменение размера
-     *  (n, value) - если размер списка меньше n, все пустые значение инициализируются value */
-    /* assign - заменить элементы списка определнным набором
-     * имеет много перегрузок */
-    // push_back, push_front - нужно указание типа данных (???)
-    // emplace_back, emplace_front - хавает все, работает как конструктор (???)
-    // emplace(pos, value) - вставляет элемент на указанную позицию
-    // insert(pos, value) - возвращает итератор на добавленный элемент (имеет много перегрузок)
-    // clear - удаляет все
-    // pop_front, pop_back
-    // erase(n) - удаляет элемент. возвращает итератор на следующий удаленному элемент или на конец списка
-    // erase(begin, end) - удаляет указанный диапозон элементов
 
+    template<typename T>
     class S21list {
-        struct Node {
-//            auto value;
-            Node *prev;
-            Node  *next;
-
-//            Node(): value(value), prev(nullptr), next(nullptr) {}
-        };
 
         public:
 
+        using value_t = T;
+
+        // === constructors === //
         S21list();
+        S21list(size_t size);
+
+        ~S21list();
+
+        // === methods === //
+        void push_front(T value);
+        void push_back(T value);
+        void pop_front();
+        void pop_back();
+
+        size_t size();
+        bool empty();
 
         private:
+
+        // === list elements === //
+        struct Node {
+            value_t value;
+            Node *prev_;
+            Node  *next_;
+        };
+
+        size_t size_;
+        Node *head_;
+        Node *tail_;
+        Node *end_;
+
+        // === other methods === //
 
     };
 }
 
 #endif // CPP_S21_CONTAINERS_SRC_S21_LIST_H
+
+
+// TODO
+// Конструкторы деструкторы
+// front back - получение крайних элементов
+// --- DONE --- size - размер списка
+// max size - в лекции(что-то под std::size_max)
+// --- DONE --- empty - пустой ли список (true/false)
+/* resize - изменение размера списка
+ *  (n) - простое изменение размера
+ *  (n, value) - если размер списка меньше n, все пустые значение инициализируются value */
+/* assign - заменить элементы списка определнным набором
+ * имеет много перегрузок */
+// --- DONE --- push_back, push_front
+// emplace_back, emplace_front - хавает все, работает как конструктор (???)
+// emplace(pos, value) - вставляет элемент на указанную позицию
+// insert(pos, value) - возвращает итератор на добавленный элемент (имеет много перегрузок)
+// clear - удаляет все
+// pop_front, pop_back
+// erase(n) - удаляет элемент. возвращает итератор на следующий удаленному элемент или на конец списка
+// erase(begin, end) - удаляет указанный диапозон элементов
+// end - ссылается на последний элемент, которого НЕ существует(он всегда должен быть пустым)
+// (begin и end работают на итераторе)
+
+// sort - менять местами узлы, а не содержимое узлов
